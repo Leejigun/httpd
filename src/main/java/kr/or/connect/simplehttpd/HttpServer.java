c9800cd9cd629f980d6a2311ac1c09fa4720da37
강경미 <carami@nate.com>
2017-08-03 22:17:23 +0900

    STEP_05 단일쓰레드로 요청 처리 완료

--- src/main/java/kr/or/connect/simplehttpd/HttpServer.java
+++ src/main/java/kr/or/connect/simplehttpd/HttpServer.java
@@ -25,15 +25,14 @@ public class HttpServer {
         try (
                 ServerSocket serverSocket = new ServerSocket(portNumber);
         ) {
-            while(true) {
-                try {
-                    Socket clientSocket = serverSocket.accept();
-                    HttpProcessThread httpProcessThread = new HttpProcessThread(clientSocket, requestHandler, responseHandler);
-                    httpProcessThread.start();
-                }catch(Exception ex){
-                    log.error("http process error : " + ex.toString());
-                }
-            }
+                Socket clientSocket = serverSocket.accept();
+
+                Request request = requestHandler.handle(clientSocket);
+                Response response = responseHandler.handle(request.getRequestTarget(), clientSocket);
+
+                DefaultServlet defaultServlet = new DefaultServlet();
+                defaultServlet.service(request, response);
+                clientSocket.close();
         } catch (IOException e) {
             System.out.println("Exception caught when trying to listen on port "
                     + portNumber + " or listening for a connection");
@@ -41,31 +40,5 @@ public class HttpServer {
             log.error(e.getMessage());
         }
 
-    }
-}
-
-class HttpProcessThread extends Thread{
-    Socket clientSocket;
-    RequestHandler requestHandler = null;
-    ResponseHandler responseHandler = null;
-
-
-    public HttpProcessThread(Socket clientSocket, RequestHandler requestHandler, ResponseHandler responseHandler){
-        this.clientSocket = clientSocket;
-        this.requestHandler = requestHandler;
-        this.responseHandler = responseHandler;
-    }
-    public void run(){
-        try {
-
-            Request request = requestHandler.handle(clientSocket);
-            Response response = responseHandler.handle(request.getRequestTarget(), clientSocket);
-
-            DefaultServlet defaultServlet = new DefaultServlet();
-            defaultServlet.service(request, response);
-            clientSocket.close();
-        }catch(Exception ex){
-            throw new RuntimeException(ex);
-        }
     }
 }
