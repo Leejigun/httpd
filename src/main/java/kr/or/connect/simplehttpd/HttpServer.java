9921ffcacacb55697c9d108114a07151a0641d1a
carami <carami@nate.com>
2017-08-04 01:01:13 +0900

    log 추가

--- src/main/java/kr/or/connect/simplehttpd/HttpServer.java
+++ src/main/java/kr/or/connect/simplehttpd/HttpServer.java
@@ -1,6 +1,5 @@
 package kr.or.connect.simplehttpd;
 
-import java.io.File;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -9,27 +8,26 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class HttpServer {
-    public static final Logger log = LoggerFactory.getLogger(HttpServer.class);
+	public static final Logger log = LoggerFactory.getLogger(HttpServer.class);
 
-    int portNumber;
+	int portNumber;
 
-    public HttpServer(int portNumber) {
-        this.portNumber = portNumber;
-    }
+	public HttpServer(int portNumber) {
+		this.portNumber = portNumber;
+	}
 
-    public void run() {
-        try (
-                ServerSocket serverSocket = new ServerSocket(portNumber);
-        ) {
-                Socket clientSocket = serverSocket.accept();
+	public void run() {
+		try (ServerSocket serverSocket = new ServerSocket(portNumber);) {
+			log.info("===================serverSocket open ===================");
+			Socket clientSocket = serverSocket.accept();
+			log.info("===================clientSocket get ====================");
+			clientSocket.close();
+			log.info("===================clientSocket close ====================");
 
-                clientSocket.close();
-        } catch (IOException e) {
-            System.out.println("Exception caught when trying to listen on port "
-                    + portNumber + " or listening for a connection");
-            System.out.println(e.getMessage());
-            log.error(e.getMessage());
-        }
+		} catch (IOException e) {
+			log.info("Exception caught when trying to listen on port " + portNumber + " or listening for a connection");
+			log.error(e.getMessage());
+		}
 
-    }
+	}
 }
