a177c8ed61f19c20bb5f68c3f48f02851db9a696
강경미 <carami@nate.com>
2017-08-03 22:19:28 +0900

    STEP_04 DefaultServlet 미완성상태

--- src/main/java/kr/or/connect/simplehttpd/DefaultServlet.java
+++ src/main/java/kr/or/connect/simplehttpd/DefaultServlet.java
@@ -7,36 +7,6 @@ import java.io.OutputStream;
 
 public class DefaultServlet {
     public void service(Request request, Response response){
-        File f = new File("webapp/" + request.getRequestTarget());
-        OutputStream out = response.getOutputStream();
-        byte[] buffer = new byte[1024];
-        int readCount = 0;
-        FileInputStream fis = null;
-        try{
-            fis = new FileInputStream(f);
-            while((readCount = fis.read(buffer)) != -1){
-                out.write(buffer,0,readCount);
-            }
-        }catch(Exception ex){
-            ex.printStackTrace();
-        }finally {
-            try {
-                fis.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            try{
-                request.getInputStream().close();
-            }catch(Exception ex){
-                ex.printStackTrace();
-            }
-            try {
-                out.close();
-            }catch(Exception ex){
-                ex.printStackTrace();
-            }
-        }
-
-
+        // request와 response를 이용하여 html문서를 읽어들여 출력하기
     }
 }
